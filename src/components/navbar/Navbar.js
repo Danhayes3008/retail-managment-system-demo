205e4df32a5e109f4d63333aa3b05cbd907a936f
danhayes <danhayes3008@outlook.com>
2021-04-30 10:49:40 +0100

    added class names to start styling the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,21 +3,21 @@
 const Navbar = () => {
     return (
         <div>
-            <nav>
-                <div>
-                    <h3>WK 32</h3>
-                    <h4>Daniel Hayes</h4>
-                    <h4>30-04-2021  10:31</h4>
+            <nav className="navbar">
+                <div className="nav-details">
+                    <h3 className="wk-nbr">WK 32</h3>
+                    <h4 className="user">Daniel Hayes</h4>
+                    <h4 className="date-time">30-04-2021  10:31</h4>
                 </div>
-                <div>
-                    <ul>
-                        <li>Schedules</li>
-                        <li>Requests</li>
-                        <li>Exceptions</li>
-                        <li>Payroll</li>
-                        <li>Forcasting</li>
-                        <li>personnel</li>
-                        <li>Logout</li>
+                <div className="nav-items">
+                    <ul className="nav-list">
+                        <li className="list-item">Schedules</li>
+                        <li className="list-item">Requests</li>
+                        <li className="list-item">Exceptions</li>
+                        <li className="list-item">Payroll</li>
+                        <li className="list-item">Forcasting</li>
+                        <li className="list-item">personnel</li>
+                        <li className="list-item">Logout</li>
                     </ul>
                 </div>
             </nav>

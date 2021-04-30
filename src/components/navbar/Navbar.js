ed9456b5c5f44a511bf17e2c9ff425da3d53211e
danhayes <danhayes3008@outlook.com>
2021-04-30 10:40:09 +0100

    added the navigation list items for my navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,6 +9,17 @@ const Navbar = () => {
                     <h4>Daniel Hayes</h4>
                     <h4>30-04-2021  10:31</h4>
                 </div>
+                <div>
+                    <ul>
+                        <li>Schedules</li>
+                        <li>Requests</li>
+                        <li>Exceptions</li>
+                        <li>Payroll</li>
+                        <li>Forcasting</li>
+                        <li>personnel</li>
+                        <li>Logout</li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )

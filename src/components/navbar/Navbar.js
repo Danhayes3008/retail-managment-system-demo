d67ca2d39bdb1becaa9c6b2819fcf771345db5d2
danhayes <danhayes3008@outlook.com>
2021-04-30 10:35:53 +0100

    added the first section of the navbar that contains the week number, user logged in, date and time

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,13 @@
 const Navbar = () => {
     return (
         <div>
-
+            <nav>
+                <div>
+                    <h3>WK 32</h3>
+                    <h4>Daniel Hayes</h4>
+                    <h4>30-04-2021  10:31</h4>
+                </div>
+            </nav>
         </div>
     )
 }

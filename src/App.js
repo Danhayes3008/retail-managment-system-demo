1f635b37b90e5991c6281dde61cec7e429e31906
danhayes <danhayes3008@outlook.com>
2021-05-01 10:41:29 +0100

    added the activity section and wrapped the navbar in a div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Activity from "./components/activity/Activity";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <div><Navbar /></div>
+      <Activity />
     </div>
   );
 }
